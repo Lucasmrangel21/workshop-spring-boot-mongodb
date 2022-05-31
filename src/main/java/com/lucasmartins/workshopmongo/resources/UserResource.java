24b69c0fa2e9cec144ae1f034bbb5fea89894ad6
lucas martins <lucas.mrangel@sempreceub.com>
2022-05-31 10:15:50 -0300

    Endpoint para retornar os posts de um usuário

--- src/main/java/com/lucasmartins/workshopmongo/resources/UserResource.java
+++ src/main/java/com/lucasmartins/workshopmongo/resources/UserResource.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.lucasmartins.workshopmongo.DTO.UserDTO;
+import com.lucasmartins.workshopmongo.domain.Post;
 import com.lucasmartins.workshopmongo.domain.User;
 import com.lucasmartins.workshopmongo.services.UserService;
 
@@ -60,5 +61,11 @@ public class UserResource {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@GetMapping("/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 }
